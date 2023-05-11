ba61da789bb9045b938dce5e351e807bbe495ed1
Mikkel Helding Vembye <B199526@prod.sitad.dk>
2023-05-11 16:03:10 +0200

    Small stuff continued

--- other/Testing functions .R
+++ other/Testing functions .R
@@ -1,5 +1,5 @@
 
 q <- "What is a carrot?"
-ask_chatgpt(q, api_key = "INSERT", sleep_time = 0, time_info = FALSE)
+ask_chatgpt(q, api_key = api_key, sleep_time = 0, time_info = FALSE)
 
-purrr::map_dfr(1:10, \(i) ask_chatgpt(q, api_key = "INSERT", sleep_time = 20, time_info = TRUE))
+purrr::map_dfr(1:10, \(i) ask_chatgpt(q, api_key = api_key, sleep_time = 0, time_info = TRUE))
