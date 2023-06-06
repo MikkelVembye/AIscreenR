f6f24300850dee2681d3fd8183ff348cb55c1dbc
Mikkel Helding Vembye <B199526@prod.sitad.dk>
2023-06-06 12:25:37 +0200

    New key test

--- tests/testthat/test-tabscreen_chatgpt.R
+++ tests/testthat/test-tabscreen_chatgpt.R
@@ -3,6 +3,6 @@ test_that("Testthat works in get_api_key()", {
 
   # skip("To avoid GHA failure")
 
-  expect_equal(get_api_key(), httr2::secret_decrypt("4UAcFSIHVz8Z4zED1WEj3k65xFBWlJ8dzavRDGG4dz0pBxEOXtvSkLwK6_fZaZqCr94oVtKBD6DQo82vwa2gljJMTw", "AISCREENR_KEY"))
+  expect_equal(get_api_key(), httr2::secret_decrypt("4UAcFSIHVz8Z4zED1WEj3k65xFBWlJ8dzavRDGG4dz0pBxEOXtvSkLwK6_fZaZqCr94oVtKBD6DQo82vwa2gljJMTw", AISCREENR_KEY))
 
 })
