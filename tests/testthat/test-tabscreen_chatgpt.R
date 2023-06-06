48e71f1c61950eacf330d07ddae1e2b43cc04d20
Mikkel Helding Vembye <B199526@prod.sitad.dk>
2023-06-06 14:33:31 +0200

    New test

--- tests/testthat/test-tabscreen_chatgpt.R
+++ tests/testthat/test-tabscreen_chatgpt.R
@@ -1,9 +1,24 @@
 
 
+
 test_that("Testthat works in get_api_key()", {
 
   skip("To avoid GHA-failure")
   key <- httr2::secret_decrypt("4UAcFSIHVz8Z4zED1WEj3k65xFBWlJ8dzavRDGG4dz0pBxEOXtvSkLwK6_fZaZqCr94oVtKBD6DQo82vwa2gljJMTw", "AISCREENR_KEY")
   expect_equal(get_api_key(), key)
+
+})
+
+
+test_that("AISCREENR_KEY works and secret_decrypt behaves properly",{
+
+  key <- httr2::secret_decrypt("4UAcFSIHVz8Z4zED1WEj3k65xFBWlJ8dzavRDGG4dz0pBxEOXtvSkLwK6_fZaZqCr94oVtKBD6DQo82vwa2gljJMTw", "AISCREENR_KEY")
+  AIscreenR:::set_api_key(key = key)
+
+  q <- "What is a carrot?"
+  answer_dat <- ask_chatgpt(q, time_info = TRUE)
+
+
+  expect_true(is.numeric(answer_dat$run_time))
 
 })
