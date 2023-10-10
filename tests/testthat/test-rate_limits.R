85c84607d3dcb8e8b993c8eb2c80e7fc91671e25
Mikkel Helding Vembye <B199526@prod.sitad.dk>
2023-10-10 10:31:56 +0200

    Further rate_limits tests

--- tests/testthat/test-rate_limits.R
+++ tests/testthat/test-rate_limits.R
@@ -8,15 +8,21 @@ test_that("General working of rate_limits_per_minute().", {
     nrow() |>
     expect_equal(2)
 
+  rlpm <- rate_limits_per_minute(model = models)
+  expect_output(print(rlpm), "tibble")
 
 })
 
-test_that("rate_limits_per_minute() casts NAs.", {
+test_that("rate_limits_per_minute() casts errors correctly.", {
 
   skip_on_cran()
 
   rlpm <- rate_limits_per_minute(model = "gpt-3")
-
   expect_identical(rlpm$model, "Error 404 [check gpt model]")
 
+  rlpm <- rate_limits_per_minute(api_key = 1234)
+  expect_identical(rlpm$model, "Error 401 Unauthorized [invalid api]")
+
 })
+
+
