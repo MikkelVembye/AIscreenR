8998d0ec1d1bae09fb4609ad63178d426e4e1231
Thomas Olsen <B375477@PROD.SITAD.DK>
2024-03-26 14:02:15 +0100

    Updated functions

--- R/scrape_price.R
+++ R/scrape_price.R
@@ -1,39 +1,35 @@
-library(rvest)
-library(dplyr)
-library(stringr)
-
 # URL of the website to scrape
 url <- "https://openai.com/pricing"
 
 # Read the HTML content of the webpage
-webpage <- read_html(url)
+webpage <- rvest::read_html(url)
 
 # Function to extract model and price information from a row
 extract_info <- function(row){
-  models <- row %>% html_nodes("td:nth-child(1) > span") %>% html_text()
-  Input_price <- row %>% html_nodes("td:nth-child(2)") %>% html_text()
-  Output_price <- row %>% html_nodes("td:nth-child(3)") %>% html_text()
+  models <- row %>% rvest::html_nodes("td:nth-child(1) > span") %>% rvest::html_text()
+  Input_price <- row %>% rvest::html_nodes("td:nth-child(2)") %>% rvest::html_text()
+  Output_price <- row %>% rvest::html_nodes("td:nth-child(3)") %>% rvest::html_text()
 
-  Input_price_d <- ifelse(str_detect(Input_price, "M"), 1e6, ifelse(str_detect(Input_price, "B"), 1e9, ifelse(str_detect(Input_price, "T"), 1e12, as.numeric(str_extract(Input_price, "/\\s*(\\d+)")))))
-  Output_price_d <- ifelse(str_detect(Output_price, "M"), 1e6, ifelse(str_detect(Output_price, "B"), 1e9, ifelse(str_detect(Output_price, "T"), 1e12, as.numeric(str_extract(Output_price, "/\\s*(\\d+)")))))
+  Input_price_d <- ifelse(stringr::str_detect(Input_price, "M"), 1e6, ifelse(stringr::str_detect(Input_price, "B"), 1e9, ifelse(stringr::str_detect(Input_price, "T"), 1e12, as.numeric(stringr::str_extract(Input_price, "/\\s*(\\d+)")))))
+  Output_price_d <- ifelse(stringr::str_detect(Output_price, "M"), 1e6, ifelse(stringr::str_detect(Output_price, "B"), 1e9, ifelse(stringr::str_detect(Output_price, "T"), 1e12, as.numeric(stringr::str_extract(Output_price, "/\\s*(\\d+)")))))
 
   # Extract only the numeric part of the price and divide by 1M
-  Input_price <- as.numeric(str_extract(Input_price, "\\d+\\.\\d+")) / Input_price_d
-  Output_price <- as.numeric(str_extract(Output_price, "\\d+\\.\\d+")) / Output_price_d
+  Input_price <- as.numeric(stringr::str_extract(Input_price, "\\d+\\.\\d+")) / Input_price_d
+  Output_price <- as.numeric(stringr::str_extract(Output_price, "\\d+\\.\\d+")) / Output_price_d
 
   data.frame(Model = models, Input = Input_price, Output = Output_price)
 }
 
 # Function to scrape and process a table
 scrape_table <- function(css_selector) {
-  rows <- webpage %>% html_nodes(css_selector)
+  rows <- webpage %>% rvest::html_nodes(css_selector)
   data <- do.call(rbind, lapply(rows, extract_info))
   # Remove the first row
   data <- data[-1, ]
   # Create variables for the input and output price for each model
   for(i in 1:nrow(data)) {
-    assign(paste0("Input_price_", gsub("-", "_", data$Model[i])), data$Input[i], envir = .GlobalEnv)
-    assign(paste0("Output_price_", gsub("-", "_", data$Model[i])), data$Output[i], envir = .GlobalEnv)
+    assign(paste0("Input_price_", stringr::str_replace_all(data$Model[i], "-", "_")), data$Input[i], envir = .GlobalEnv)
+    assign(paste0("Output_price_", stringr::str_replace_all(data$Model[i], "-", "_")), data$Output[i], envir = .GlobalEnv)
   }
 }
 
