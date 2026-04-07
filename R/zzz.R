0674897cc9fc4590041afa2719ddb3acdc31bdc9
Mikkel Vembye <mikkel.vembye@gmail.com>
2026-04-07 08:36:21 +0200

    Loading text changed

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,8 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage(paste0("Welcome to the AIscreenR. Be aware ",
   "that we do not take\nany responsibility for the cost of using this software.\n\n",
-  "If you experience any breakdowns, check: https://status.openai.com/")
+  "If you experience any breakdowns with OpenAI's API models,\n",
+  "check: https://status.openai.com/")
   )
 }
 
